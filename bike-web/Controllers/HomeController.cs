117fa4a9ba48830e8cb065a402d8a231af2c4743
love1414332@gmail.com <love1414332@gmail.com>
2021-10-18 16:12:31 +0800

    加入專案檔案。

---
+++ bike-web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bike_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult officialRoute()
+        {
+            return View();
+        }
+
+        public ActionResult privateRoute()
+        {
+
+            return View();
+        }
+
+        public ActionResult memberPage()
+        {
+
+            return View();
+        }
+
+        public ActionResult routeEditPage()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ bike-web/ViewModels/DBConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bike_web.ViewModels
+{
+    public class DBConnectionString
+    {
+        /*請記得每次都要來這邊改連線字串*/
+        public static string DBConnectWang = "";
+    }
+}
