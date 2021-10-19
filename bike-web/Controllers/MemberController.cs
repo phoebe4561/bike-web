5aa4a1060e48288d43ee19be087d22c167de05d7
love1414332@gmail.com <love1414332@gmail.com>
2021-10-19 20:14:21 +0800

    註冊頁面

--- bike-web/Controllers/MemberController.cs
+++ bike-web/Controllers/MemberController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using bike_web.Models;
+using bike_web.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,25 @@ namespace bike_web.Controllers
     public class MemberController : Controller
     {
         // GET: Member
-        public ActionResult Index()
+        public ActionResult Login()
         {
-            return View();
+
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Register(string Name,string account,string password,DateTime date)
+        {
+            KSBikeEntities db = new KSBikeEntities();
+            user a = new user();
+            var date1 = String.Format("{0:yyyy/MM/dd}",date);
+            date = Convert.ToDateTime(date1);
+            a.username = Name;
+            a.email = account;
+            a.password = password;
+            a.birthday = date;
+            db.users.Add(a);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- bike-web/ViewModels/RegisterViewModels.cs
+++ bike-web/ViewModels/RegisterViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bike_web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,6 @@ namespace bike_web.ViewModels
 {
     public class RegisterViewModels
     {
-
+        public user Users { get; set;}
     }
 }
