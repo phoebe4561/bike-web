596e2d93ed0908287518dd4162bafb552aa67d8c
phoebe <phoebe4561@gmail.com>
2021-10-24 22:28:22 +0800

    git merge problem

--- bike-web/Controllers/HomeController.cs
+++ bike-web/Controllers/HomeController.cs
@@ -10,8 +10,6 @@ namespace bike_web.Controllers
 {
     public class HomeController : Controller
     {
-        private object db;
-
         public ActionResult Index()
         {
             KSBikeEntities db = new KSBikeEntities();
@@ -29,10 +27,43 @@ namespace bike_web.Controllers
                             oc_allStar=oc.all_star_summary,
                         }).OrderByDescending(e => e.oc_allStar).Take(3).ToList();
 
+            //List<popularRoute> 所有官方路線 = new List<popularRoute>();
+
+            //string keyword = Request.Form["txtKeyword"];
+            //if (string.IsNullOrEmpty(keyword))
+            //    所有官方路線 = (from h in db.Homes
+            //              join oc in db.official_route_comment on h.id equals oc.article_title_id
+            //              select new popularRoute
+            //              {
+            //                  h_ID = h.id,
+            //                  h_description = h.hdescription,
+            //                  h_name = h.hname,
+            //                  h_distance = (double?)h.hdistance,
+            //                  h_rank = h.hrank,
+            //                  h_img = h.himg,
+            //                  oc_artitleTitleID = oc.article_title_id,
+            //                  oc_allStar = oc.all_star_summary,
+            //              }).OrderBy(e => e.h_ID).ToList();
+            //else
+            //    所有官方路線 = (from h in db.Homes
+            //              join oc in db.official_route_comment on h.id equals oc.article_title_id
+            //              where h.hname.Contains(keyword)
+            //              select new popularRoute
+            //              {
+            //                  h_ID = h.id,
+            //                  h_description = h.hdescription,
+            //                  h_name = h.hname,
+            //                  h_distance = (double?)h.hdistance,
+            //                  h_rank = h.hrank,
+            //                  h_img = h.himg,
+            //                  oc_artitleTitleID = oc.article_title_id,
+            //                  oc_allStar = oc.all_star_summary,
+            //              }).OrderBy(e => e.h_ID).ToList();
+
             var 所有官方路線 = (from h in db.Homes
-                         join oc in db.official_route_comment on h.id equals oc.article_title_id
-                         select new popularRoute
-                         {
+                          join oc in db.official_route_comment on h.id equals oc.article_title_id
+                          select new popularRoute
+                          {
                               h_ID = h.id,
                               h_description = h.hdescription,
                               h_name = h.hname,
@@ -77,16 +108,6 @@ namespace bike_web.Controllers
             KSBikeEntities db = new KSBikeEntities();
 
             var off_routes = db.Homes.OrderBy(m => m.id).ToList();
-
-//            var off_routes = (from o in db.official_route_data
-//                              join p in db.private_route
-//on o.id equals p.user_id
-//                              select new
-//                              {
-//                                  article = p.article_title,
-//                              }).ToList();
-
-
             return View(off_routes);
         }
 
