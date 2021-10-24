fa5649b057774be9e97736e1c974eb946973ce5f
phoebe <phoebe4561@gmail.com>
2021-10-24 10:49:30 +0800

    fetch weather api and revise views index

---
+++ bike-web/ViewModels/KeyWordHomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace bike_web.ViewModels
+{
+    public class KeyWordHomeViewModel
+    {
+        [Required]
+        public string SearchKey { get; set; }
+    }
+}
