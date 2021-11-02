0fbd6a3c79edcae4c8734ba23c11ff665a73af67
phoebe <phoebe4561@gmail.com>
2021-11-02 13:53:11 +0800

    add offRoute Controller and View

---
+++ bike-web/ViewModels/offRouteViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace bike_web.ViewModels
+{
+    public class offRouteViewModel
+    {
+        public List<offRoute> offcialAllRoute { get; set; }
+        public List<offRoute> offcialLowRoute { get; set; }
+        public List<offRoute> offcialMiddleRoute { get; set; }
+        public List<offRoute> offcialHighRoute { get; set; }
+        public string searchKeyword { get; set; }
+        public string searchRank { get; set; }
+        public double searchDistance { get; set; }
+    }
+
+    public class offRoute
+    {
+        public int h_ID { get; set; }
+        public string h_name { get; set; }
+        [DisplayName("路線名稱")]
+        public string h_description { get; set; }
+        [DisplayName("路線介紹")]
+        public string h_rank { get; set; }
+        [DisplayName("路線分級")]
+        public double? h_distance { get; set; }
+        [DisplayName("路線距離")]
+        public string h_img { get; set; }
+        [DisplayName("路線照片")]
+        public double? oc_allStar { get; set; }
+        [DisplayName("星星評分")]
+        public int oc_artitleTitleID { get; set; }
+    }
+}
